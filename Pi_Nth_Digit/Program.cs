2566c074f257413c0e1322570747378aff11b1c3
Elliot <braemelliot@gmail.com>
2018-04-17 21:06:15 -0500

    finished project

--- Pi_Nth_Digit/Program.cs
+++ Pi_Nth_Digit/Program.cs
@@ -6,7 +6,26 @@ namespace Pi_Nth_Digit
     {
         static void Main(string[] args)
         {
-            
+            double pi = Math.PI;
+            string input;
+            int value = -1;
+        prompt:
+            do
+            {
+                Console.WriteLine("This program prints Pi up to the specified decimal.");
+                Console.WriteLine("Enter in a specified decimal between 0 and 15");
+                input = Console.ReadLine();
+            } while (!int.TryParse(input, out value));
+
+            if (value < 0 || value > 15) {
+                Console.WriteLine("You entered {0}", value);
+                Console.WriteLine("Please specify a decimal between 0 and 15.");
+                Console.WriteLine();
+                goto prompt;
+            }
+
+            String result = Math.Round(pi, value).ToString();
+            Console.WriteLine(result);
         }
     }
 }
