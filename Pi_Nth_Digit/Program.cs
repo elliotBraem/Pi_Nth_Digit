3a14eddc8f074eb373cfc3bbe25b287c1a885684
Elliot <braemelliot@gmail.com>
2018-04-17 20:24:42 -0500

    first commit

---
+++ Pi_Nth_Digit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pi_Nth_Digit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
